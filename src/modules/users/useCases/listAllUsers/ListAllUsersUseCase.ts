a50babdd28c6d2ae25143a73696a63897bb99611
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-12-17 01:05:00 -0400

    feat: route to list users when user has admin permissions

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,12 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const sessionUser = this.usersRepository.findById(user_id);
+    if (!sessionUser.admin) {
+      throw new Error("User not allowed to list users");
+    }
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
